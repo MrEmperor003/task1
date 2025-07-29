5fab0d0d8c3c42d6f4913718d1b89658eb74bd2a
Mr Emperor <mremperor003@gmail.com>
2025-07-29 22:08:37 +0000

    initail commit

---
+++ task1.py
@@ -0,0 +1,20 @@
+# create 6 ouple containing strings, int,float,list,tuples and mixed data type.
+
+# 1. Tuple with Strings
+tuple_strings = ("Apple", "Banana", "Cherry")
+
+# 2. Tuple with Integers
+tuple_integers = (10, 20, 30, 40)
+
+# 3. Tuple with Floats
+tuple_floats = (3.14, 2.71, 1.41)
+
+# 4. Tuple with Lists inside
+tuple_with_lists = ([1, 2, 3], ["a", "b", "c"])
+
+# 5. Tuple with Nested Tuples
+tuple_with_tuples = ((1, 2), (3, 4), (5, 6))
+
+# 6. Tuple with Mixed Data Types
+tuple_mixed = ("Hello", 25, 3.14, [1, 2], (5, 6))
+
